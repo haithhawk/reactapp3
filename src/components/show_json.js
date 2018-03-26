fa514c78e1a9913a6700f3cca9ba79601ed542cc
Haitham <haithhawk@gmail.com>
2018-03-27 01:11:23 +0300

    reading data from json and showing it

--- src/components/show_json.js
+++ src/components/show_json.js
@@ -1,36 +1,48 @@
 import React, { Component } from 'react';
 import Data from '../data';
 
-class ShowJson extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      people: []
-    };
-  }
-  componentDidMount() {
-    fetch('./Data')
-      .then(results => {
-        return results.json();
-      })
-      .then(data => {
-        let people = data.results.map(person => {
-          return (
-            <ul key={person.results}>
-              <li>{person.first_name}</li>
-            </ul>
-          );
-        });
-        this.setState({ people: people });
-        console.log('state', this.state.people);
-      });
-  }
+// class ShowJson extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       people: []
+//     };
+//   }
+//   componentDidMount() {
+//     fetch('./Data')
+//       .then(results => {
+//         return results.json();
+//       })
+//       .then(data => {
+//         let people = data.results.map(person => {
+//           return (
+//             <ul key={person.results}>
+//               <li>{person.first_name}</li>
+//             </ul>
+//           );
+//         });
+//         this.setState({ people: people });
+//         console.log('state', this.state.people);
+//       });
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <div>{this.state.people}</div>
+//       </div>
+//     );
+//   }
+// }
 
+class ShowJson extends Component {
   render() {
     return (
-      <div>
-        <div>{this.state.people}</div>
-      </div>
+      <ul>
+        {Data.map(person => {
+          return <li key={person.id}>{person.first_name}</li>;
+        })}
+      </ul>
     );
   }
 }
